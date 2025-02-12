384076e6dda71691c9888f48fb428082c0251882
Ojrb11113 <ramirezbelloorlandojoaquin@gmail.com>
2025-02-12 16:34:12 -0400

    algo mas12

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AllExceptionFilter } from './common/utils/all-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: 'http://localhost:5173/',
     credentials: true
   });
   app.use(cookieParser());
